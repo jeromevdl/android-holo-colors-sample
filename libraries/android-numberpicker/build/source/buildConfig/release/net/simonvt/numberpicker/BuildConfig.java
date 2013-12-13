f28796c188d227c44308abdca16367cf5f855329
Jerome Van Der Linden <jeromevdl@gmail.com>
2013-12-14 00:37:37 +0100

    version play store

---
+++ libraries/android-numberpicker/build/source/buildConfig/release/net/simonvt/numberpicker/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.simonvt.numberpicker;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+
+}
---
+++ libraries/switch-backport/build/source/buildConfig/debug/org/jraf/android/backport/switchwidget/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.jraf.android.backport.switchwidget;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
