0bfe32dd262af0411d11e07f2180648ecfc36318
Jerome Van Der Linden <jeromevdl@gmail.com>
2013-12-15 23:51:46 +0100

    clean

--- libraries/android-numberpicker/build/source/buildConfig/debug/net/simonvt/numberpicker/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.simonvt.numberpicker;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
--- libraries/switch-backport/build/source/buildConfig/debug/org/jraf/android/backport/switchwidget/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.jraf.android.backport.switchwidget;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
