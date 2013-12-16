7730b625d974c873d17cd4d3060bc9bdff14a6f9
Jerome Van Der Linden <jeromevdl@gmail.com>
2013-12-16 22:39:44 +0100

    remove unused import

--- AndroidHoloColors/src/main/java/fr/vdl/androidholocolors/activity/ListActivity.java
+++ AndroidHoloColors/src/main/java/fr/vdl/androidholocolors/activity/ListActivity.java
@@ -1,7 +1,6 @@
 package fr.vdl.androidholocolors.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
